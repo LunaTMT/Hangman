5748bcfb2072218769e1b99aa324a412b629be6d
T Shredder <44672093+LunaTMT@users.noreply.github.com>
2023-06-09 17:33:40 +0100

    Update display.py

--- display.py
+++ display.py
@@ -4,14 +4,14 @@ clear = lambda: os.system('clear')
 class Display:
 
     def __init__(self, hangman) -> None:
-        self.HANGMAN = hangman
+        self.hangman = hangman
         self.player = hangman.player
         self.word = hangman.word
           
     def __str__ (self) -> str:
         clear()
         return f""" 
-                {self.HANGMAN.show()} 
+                {self.hangman.show()} 
                 {self.word.show_hidden()} 
                 {self.player}"""
 
